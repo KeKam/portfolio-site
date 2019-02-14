8da18df1cdf06cb877cccc6e4795ebf1985e20b3
Kenta Kamimura <kenta.stolt.kamimura@gmail.com>
2019-02-14 18:00:26 +0100

    Add navigation component

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  } from 'reactstrap';
+
+class Navigation extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color='light' light expand='md'>
+          <NavbarBrand href='/'>
+            KENTA KAMIMURA
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className='ml-auto' navbar>
+              <NavItem>
+                <NavLink href='#about'>
+                  ABOUT ME
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href='#projects'>
+                  PROJECTS
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href='#contact'>
+                  CONTACT
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default Navigation;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 import React from "react"
+import Navigation from '../components/Navigation/Navigation';
+import 'bootstrap/dist/css/bootstrap.css';
 
-export default () => <div>Hello world!</div>
+const HomePage = () => {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+}
+
+export default HomePage;

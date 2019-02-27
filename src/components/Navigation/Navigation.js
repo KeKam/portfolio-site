e045d833ee3b9886deebd9145d92e0df25a5fb1a
Kenta Kamimura <kenta.stolt.kamimura@gmail.com>
2019-02-27 15:09:21 +0100

    Add some scroll functionality to navigation bar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,13 +8,15 @@ import {
   NavItem,
   NavLink,
   } from 'reactstrap';
+import './Navigation.css';
 
 class Navigation extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      isOpen: false
+      isOpen: false,
+      hasScrolledDown: false
     };
   }
   toggle = () => {
@@ -22,33 +24,53 @@ class Navigation extends Component {
       isOpen: !this.state.isOpen
     });
   }
+
+  handleScroll = () => {
+    const bodyScrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    let scrolledDown = bodyScrollTop > 75 ? true : false;
+    this.setState({
+      hasScrolledDown: scrolledDown
+    });
+  }
+
+  componentDidMount = () => {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+  
+  componentWillUnmount = () => {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
   render() {
+    const { hasScrolledDown } = this.state;
+    const backgroundColor = hasScrolledDown ? 'black-background' : '';
+    const navFontColor = hasScrolledDown ? 'white-font' : 'black-font';
     return (
       <div>
-        <Navbar color='light' light expand='md'>
-          <NavbarBrand href='/'>
+        <Navbar className={backgroundColor} fixed={'top'} light expand='md'>
+          <NavbarBrand className={navFontColor} href='/'>
             KENTA KAMIMURA
           </NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
+          <NavbarToggler className={navFontColor} onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className='ml-auto' navbar>
               <NavItem>
-                <NavLink href='#about'>
+                <NavLink className={navFontColor} href='#about'>
                   ABOUT ME
                 </NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href='#skillset'>
+                <NavLink className={navFontColor} href='#skillset'>
                   SKILLSET
                 </NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href='#projects'>
+                <NavLink className={navFontColor} href='#projects'>
                   PROJECTS
                 </NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href='#contact'>
+                <NavLink className={navFontColor} href='#contact'>
                   CONTACT
                 </NavLink>
               </NavItem>

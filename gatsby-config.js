2bdcdbb9e724bae466e6630ccd0590f0af8b5fa8
Kenta Kamimura <kenta.stolt.kamimura@gmail.com>
2019-03-06 14:23:42 +0100

    Add gatsby-config.js

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      }
+    },  
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ]
+}

3478e301493d79366ece46873cfe589f33480284
Kenta Kamimura <kenta.stolt.kamimura@gmail.com>
2019-04-03 16:16:40 +0200

    Update contact component and add styling

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import contactData from '../../data/contacts.json';
+import './Contact.css';
 
 const Contact = () => {
   return (
     <section id='contact' className='section'>
       <h2 className='text-center'>CONTACT</h2>
-      <div className='section-content'>
-        {contactData.map(contact => (
-          <div className="" key={contact.url}>
-            <a
-              href={contact.url}
-              target="_blank"
-              title={`External link to my ${contact.name} account`}
-              className=""
-              rel="noopener noreferrer"
-            >
-              <i
-                className={`fa fa-${contact.icon}`}
-                alt={`External link to my ${contact.name} account`}
-              />
-            </a>
-          </div>
-        ))} 
+      <h3 className='text-center'>Have any questions? Feel free to contact me!</h3>
+      <div className='contact-list'>
+        <hr className='hr-sections' />
+        <div className='contacts'>
+          {contactData.map(contact => (
+            <div className='contact-item' key={contact.url}>
+              <a
+                href={contact.url}
+                target="_blank"
+                title={`Link to my ${contact.name} account`}
+                className='contact-link'
+                rel="noopener noreferrer"
+              >
+                <i
+                  className={`fa fa-${contact.icon} fa-2x`}
+                  alt={`Link to my ${contact.name} account`}
+                />
+                <h3>{contact.name}</h3>
+              </a>
+            </div>
+          ))} 
+        </div>
       </div>
     </section>
   );

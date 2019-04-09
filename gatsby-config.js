a538289a3b78eca300c8cc9c8a1798786573d449
Kenta Kamimura <kenta.stolt.kamimura@gmail.com>
2019-04-09 19:21:58 +0200

    Add manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,18 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Kenta Kamimura`,
+        short_name: `KentaKamimura`,
+        start_url: `/`,
+        background_color: `#000000`,
+        theme_color: `#000000`,
+        display: `standalone`,
+        icon: `src/images/icon.png` // This path is relative to the root of the site.
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `data`,
@@ -22,6 +34,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sitemap`,
-    `gatsby-plugin-react-helmet`
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-manifest`
   ]
 }

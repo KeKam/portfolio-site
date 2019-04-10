43bc64dcfa877b729336216ae1fbf3f7c934b3cb
Kenta Kamimura <kenta.stolt.kamimura@gmail.com>
2019-04-10 16:47:17 +0200

    Add missing brackets to banner component

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,25 +3,28 @@ import Img from 'gatsby-image';
 import Scrollschor from 'react-scrollchor';
 import './Banner.css';
 
-const Banner = ({ bannerImg }) =>
-<div className='banner'>
-  <Img
-    title='Banner image'
-    alt='Image of a laptop'
-    fluid={bannerImg.childImageSharp.fluid}
-    className='banner-img'
-  />
-  <div className='overlay' />
-  <div className='center'>
-    <h1 className='banner-header'>KENTA KAMIMURA</h1>
-    <hr className='hr-line'/>
-    <p className='banner-text'>WEB DEVELOPER</p>
-  </div>
-  <div className='arrow-container'>
-    <Scrollschor to='#about' aria-label='Jump to about section' animate={{ duration: 600 }} className='arrow-button'>
-      <i className='fa fa-angle-double-down'/>
-    </Scrollschor>
-  </div>
-</div>
+const Banner = ({ bannerImg }) => {
+  return (
+    <div className='banner'>
+      <Img
+        title='Banner image'
+        alt='Image of a laptop'
+        fluid={bannerImg.childImageSharp.fluid}
+        className='banner-img'
+      />
+      <div className='overlay' />
+      <div className='center'>
+        <h1 className='banner-header'>KENTA KAMIMURA</h1>
+        <hr className='hr-line'/>
+        <p className='banner-text'>WEB DEVELOPER</p>
+      </div>
+      <div className='arrow-container'>
+        <Scrollschor to='#about' aria-label='Jump to about section' animate={{ duration: 600 }} className='arrow-button'>
+          <i className='fa fa-angle-double-down'/>
+        </Scrollschor>
+      </div>
+    </div>
+  );
+}
 
 export default Banner; 

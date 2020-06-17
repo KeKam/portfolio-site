dc65366482d4265b331e204dc30481b4a6f968c0
Kenta Kamimura <kenta.stolt.kamimura@gmail.com>
2020-06-17 13:38:24 +0200

    Add new Toolbox logo and tweak description for projects

--- src/images/logos/logos.js
+++ src/images/logos/logos.js
@@ -15,93 +15,99 @@ import expressLogo from './express-logo.svg';
 import gitLogo from './git-logo.svg';
 import bootstrapLogo from './bootstrap-logo.svg';
 import vscodeLogo from './vscode-logo.svg';
+import typescriptLogo from './typescript-logo.svg';
 
 const Logos = [
   {
     id: 1,
     src: htmlLogo,
-    alt: 'Html logo'
+    alt: 'Html logo',
   },
   {
     id: 2,
     src: cssLogo,
-    alt: 'Css logo'
+    alt: 'Css logo',
   },
   {
     id: 3,
     src: javascriptLogo,
-    alt: 'Javascript logo'
+    alt: 'Javascript logo',
   },
   {
     id: 4,
     src: sassLogo,
-    alt: 'Sass logo'
+    alt: 'Sass logo',
   },
   {
     id: 5,
     src: reactLogo,
-    alt: 'React logo'
+    alt: 'React logo',
   },
   {
     id: 6,
     src: reduxLogo,
-    alt: 'Redux logo'
+    alt: 'Redux logo',
   },
   {
     id: 7,
     src: gatsbyLogo,
-    alt: 'Gatsby logo'
+    alt: 'Gatsby logo',
   },
   {
     id: 8,
     src: graphqlLogo,
-    alt: 'GraphQL logo'
+    alt: 'GraphQL logo',
   },
   {
     id: 9,
     src: nodeLogo,
-    alt: 'Nodejs logo'
+    alt: 'Nodejs logo',
   },
   {
     id: 10,
     src: mongodbLogo,
-    alt: 'MongoDB logo'
+    alt: 'MongoDB logo',
   },
   {
     id: 11,
     src: firebaseLogo,
-    alt: 'Firebase logo'
+    alt: 'Firebase logo',
   },
   {
     id: 12,
     src: postgresqlLogo,
-    alt: 'PostgreSQL logo'
+    alt: 'PostgreSQL logo',
   },
   {
     id: 13,
     src: webpackLogo,
-    alt: 'Webpack logo'
+    alt: 'Webpack logo',
   },
   {
     id: 14,
     src: expressLogo,
-    alt: 'Express logo'
+    alt: 'Express logo',
   },
   {
     id: 15,
     src: gitLogo,
-    alt: 'Git logo'
+    alt: 'Git logo',
   },
   {
     id: 16,
     src: bootstrapLogo,
-    alt: 'Bootstrap logo'
+    alt: 'Bootstrap logo',
   },
   {
     id: 17,
     src: vscodeLogo,
-    alt: 'Visul Studio Code logo'
-  }
-]
+    alt: 'Visul Studio Code logo',
+  },
+  {
+    id: 18,
+    src: typescriptLogo,
+    alt: 'TypeScript logo',
+  },
+];
 
 export default Logos;
